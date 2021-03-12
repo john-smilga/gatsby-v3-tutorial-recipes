7b793ad3dc276750f4c501020039c47fc6f37c1c
john-smilga <johnsmilga@gmail.com>
2021-03-12 08:53:20 -0800

    more error page testing

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,10 @@
 import React from "react"
-import RecipesList from "../components/RecipesList"
 const Error = () => {
   return (
-    <main className="page">
-      <section className="error-page">
+    <main className="error-page">
+      <section>
         <h1>404</h1>
         <h3>page not found</h3>
-      </section>
-      <section className="error-recipes">
-        <p>Cronut distillery pork belly offal cred food truck.</p>
-        <RecipesList />
       </section>
     </main>
   )
